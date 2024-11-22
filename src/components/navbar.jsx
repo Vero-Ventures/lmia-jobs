dcddaab6e5fd854074598711b8ea769091283f09
Michael Lei <michaelleie0ca@gmail.com>
2024-11-22 11:16:12 -0800

    Fix prettier errors

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 function Navbar({ links }) {
   return (
-    <header className="px-4 lg:px-6 h-14 flex items-center border-y">
+    <header className="flex h-14 items-center border-y px-4 lg:px-6">
       <Link className="flex items-center justify-center" href="/">
         <BriefcaseIcon className="h-6 w-6" />
         <span className="sr-only">Job Posting Site</span>
@@ -12,8 +12,9 @@ function Navbar({ links }) {
         {links.map((link, index) => (
           <Link
             key={index}
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href={link.url}>
+            className="text-sm font-medium underline-offset-4 hover:underline"
+            href={link.url}
+          >
             {link.text}
           </Link>
         ))}
@@ -34,7 +35,8 @@ function BriefcaseIcon(props) {
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
-      strokeLinejoin="round">
+      strokeLinejoin="round"
+    >
       <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16" />
       <rect width="20" height="14" x="2" y="6" rx="2" />
     </svg>
