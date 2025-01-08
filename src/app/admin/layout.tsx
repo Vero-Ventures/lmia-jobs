6cf1bc9d3adae5733c5651ac6c94527e545f7f48
B. Rogers <bradenrogersdev@gmail.com>
2025-01-08 12:33:38 -0800

    Create pages and layout for dashboard basic elements.

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,37 @@
+import "../globals.css";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "LMIA Jobs",
+  description: "Streamlining the LMIA process for Canadian employers",
+};
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const links = [
+    { text: "Dashboard", url: "/dashboard" },
+    { text: "New Post", url: "dashboard/post" },
+    { text: "Account", url: "/dashboard/account" },
+    { text: "Log Out", url: "/api/auth/login" },
+  ];
+  return (
+    <div className={cn(inter.className, "antialiased")}>
+      <Navbar links={links} />
+      {children}
+      <Toaster />
+      <Footer />
+    </div>
+  );
+}
