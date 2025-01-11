ca1b34d09cfe9b4167dbccabd819204352ff62dd
scottchen98 <scott.chen1117@gmail.com>
2025-01-10 20:10:28 -0800

    Update login link to use correct URL path

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -9,7 +9,7 @@ export default function Component() {
     { text: "Pricing", url: "/pricing" },
     { text: "About", url: "/about-us" },
     { text: "Join Now", url: "/sign-up" },
-    { text: "Log In", url: "/log-in" },
+    { text: "Log In", url: "/sign-in" },
   ];
 
   return (
@@ -34,7 +34,7 @@ export default function Component() {
             <Button
               type="submit"
               className="mt-8 w-64 bg-blue-600 bg-gradient-to-r from-blue-500 to-blue-600 py-8 text-2xl font-bold text-white shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:bg-blue-700 hover:from-blue-700 hover:to-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 sm:mt-4">
-              <Link href={"/log-in"}>Log In</Link>
+              <Link href={"/sign-in"}>Log In</Link>
             </Button>
           </div>
         </main>
