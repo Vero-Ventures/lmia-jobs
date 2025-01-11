abb42307134805f77e0dfe6af8e23666bebf886a
scottchen98 <scott.chen1117@gmail.com>
2025-01-10 17:16:17 -0800

    Add account component to display user information

--- src/app/admin/dashboard/account/page.tsx
+++ src/app/admin/dashboard/account/page.tsx
@@ -1,19 +1,21 @@
-import { WrenchIcon } from "lucide-react";
-// import { authClient } from "@/lib/auth-client";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import Account from "./account";
+import { unauthorized } from "next/navigation";
 
-export default function Component() {
-  // const session = authClient.useSession();
+export default async function Component() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    unauthorized();
+  }
+
   return (
-    <div>
-      <div className="flex h-screen w-full flex-col items-center justify-center gap-4 bg-gray-100">
-        <WrenchIcon className="h-12 w-12 text-gray-500" />
-        <h1 className="text-2xl font-bold text-gray-800">
-          Page Under Construction
-        </h1>
-        <p className="max-w-md text-center text-gray-500">
-          This page is currently being worked on and will be available soon.
-          Please check back later.
-        </p>
+    <div className="flex h-screen w-full bg-gray-100">
+      <div className="mx-auto mt-10 w-[80%]">
+        <Account name={session.user.name} email={session.user.email} />
       </div>
     </div>
   );
