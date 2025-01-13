665b3d9bd0de8dcf47cda4642216ec7f2b263f5b
B. Rogers <bradenrogersdev@gmail.com>
2025-01-13 11:27:28 -0800

    Simplify mailer

--- src/actions/mailer.tsx
+++ src/actions/mailer.tsx
@@ -125,3 +125,19 @@ export async function sendInvitesAndReminders(
     return;
   }
 }
+
+export async function optOutOfReminders(email: string): Promise<string> {
+  const optedOutUser = await db
+    .select()
+    .from(user)
+    .where(eq(user.email, email))
+    .then((res) => res[0]);
+
+  if (!optedOutUser) {
+    throw new Error("User with that email could not be found.");
+  }
+
+  await db.update(user).set({ optedOut: true }).where(eq(user.email, email));
+
+  return "true";
+}
