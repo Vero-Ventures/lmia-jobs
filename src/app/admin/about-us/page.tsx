b66f19ad36ef0fa14251e6a361b5c3881e987920
B. Rogers <bradenrogersdev@gmail.com>
2025-01-16 15:23:37 -0800

    Remove redundant cookie policy page and added about us page through mdx display.

--- src/app/admin/about-us/page.tsx
+++ src/app/admin/about-us/page.tsx
@@ -1,4 +1,4 @@
-import TermsOfService from "@/app/admin/privacy-policy/terms-of-service.mdx";
+import AboutUs from "@/app/admin/privacy-policy/terms-of-service.mdx";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import Navbar from "@/components/navbar";
@@ -18,7 +18,7 @@ export default async function Page() {
     <div className="flex min-h-dvh flex-col bg-gradient-to-br from-blue-50 via-blue-100 to-white">
       <Navbar links={session ? SessionLinks : BaseLinks} />
       <div className="mx-auto my-8 w-2/3 flex-grow rounded-lg bg-white px-8 py-10">
-        <TermsOfService components={components} />
+        <AboutUs components={components} />
       </div>
       <Footer />
     </div>
