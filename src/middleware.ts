8855a971c7c3ae80bda390e2eaa4552f308634aa
B. Rogers <bradenrogersdev@gmail.com>
2025-01-17 14:03:01 -0800

    test removing middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,26 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const domainName = req.headers.get("host") || "";
+export function middleware(_req: NextRequest) {
+  // const domainName = req.headers.get("host") || "";
 
-  const validRoutes: Record<string, string> = {
-    "manageopportunities.ca": "/admin",
-    "accessibleopportunities.ca": "/disability-job-board",
-    "asylumopportunities.ca": "/asylum-job-board",
-    "indigenousopportunities.ca": "/indigenous-job-board",
-    "immigrantopportunities.ca": "/newcomers-job-board",
-    "youthopportunities.ca": "/youth-job-board",
-  };
+  // const validRoutes: Record<string, string> = {
+  //   "manageopportunities.ca": "/admin",
+  //   "accessibleopportunities.ca": "/disability-job-board",
+  //   "asylumopportunities.ca": "/asylum-job-board",
+  //   "indigenousopportunities.ca": "/indigenous-job-board",
+  //   "immigrantopportunities.ca": "/newcomers-job-board",
+  //   "youthopportunities.ca": "/youth-job-board",
+  // };
 
-  const targetPath = validRoutes[domainName];
-  const baseRoute = req.nextUrl.pathname.split("/")[1];
-  const formattedBaseRoute = `/${baseRoute}`;
+  // const targetPath = validRoutes[domainName];
+  // const baseRoute = req.nextUrl.pathname.split("/")[1];
+  // const formattedBaseRoute = `/${baseRoute}`;
 
-  console.log("Domain: " + domainName);
-  console.log("Target Path: " + targetPath);
-  console.log("Base Route: " + baseRoute);
-  console.log("Formatted Base Route: " + formattedBaseRoute);
-
-  if (targetPath && formattedBaseRoute !== targetPath) {
-    const url = new URL(validRoutes[domainName], req.nextUrl.origin);
-    return NextResponse.redirect(url);
-  }
+  // if (targetPath && formattedBaseRoute !== targetPath) {
+  //   const url = new URL(validRoutes[domainName], req.nextUrl.origin);
+  //   return NextResponse.redirect(url);
+  // }
 
   return NextResponse.next();
 }
