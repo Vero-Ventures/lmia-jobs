e46c18752c9215a0ec9c892282fdda7e41099674
B. Rogers <bradenrogersdev@gmail.com>
2025-01-22 10:03:55 -0800

    Pricing page update.

--- src/app/admin/pricing/page.tsx
+++ src/app/admin/pricing/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import { authClient } from "@/lib/auth-client";
-import { redirect } from "next/navigation";
 import { useState } from "react";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { BaseLinks, SessionLinks } from "@/app/admin/dashboard/lib/constants";
-import Form from "next/form";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { createStripeCheckout } from "@/actions/stripe/create-checkout";
 import PricingInfo from "@/app/admin/pricing/pricing-info.mdx";
 
 export default function Page() {
@@ -18,48 +14,28 @@ export default function Page() {
   const [postBoards, setPostBoards] = useState(3);
   const [postTime, setPostTime] = useState(4);
 
-  const [createSessionError, setCreateSessionError] = useState(false);
-
-  const submitPurchaseForm = async () => {
-    if (!session) {
-      redirect("/admin/sign-in");
-    }
-
-    setCreateSessionError(false);
-
-    const result = await createStripeCheckout(
-      session!.user.email,
-      postBoards,
-      postTime
-    );
-
-    if (result.result) {
-      redirect(result.url);
-    } else {
-      setCreateSessionError(true);
-    }
-  };
-
   const components = {
     h1: (props: string[]) => (
       <p
-        className="mb-4 mt-2 text-center text-xl font-bold mb:text-2xl sm:text-3xl"
+        className="mb-2 mt-4 text-center text-xl font-bold mb:text-2xl sm:text-3xl"
+        {...props}
+      />
+    ),
+    h2: (props: string[]) => (
+      <p
+        className="mt-8 text-center text-xl font-bold mb:text-2xl sm:text-3xl"
         {...props}
       />
     ),
     h3: (props: string[]) => (
       <p
-        className="mx-auto max-w-80 px-4 text-center text-lg xl:max-w-full"
+        className="mx-auto max-w-72 p-4 text-center text-xl font-semibold xl:max-w-full"
         {...props}
       />
     ),
-    h4: (props: string[]) => (
-      <p
-        className="mx-auto max-w-72 p-4 text-center text-lg font-semibold xl:max-w-full"
-        {...props}
-      />
+    p: (props: string[]) => (
+      <p className="mt-4 px-6 text-lg leading-8" {...props} />
     ),
-    p: (props: string[]) => <p className="leading-8" {...props} />,
   };
 
   return (
@@ -68,56 +44,11 @@ export default function Page() {
       <div className="flex flex-grow flex-col justify-center md:flex-row md:justify-evenly md:px-6 md:py-8 lg:mx-auto lg:w-[1024px] xl:w-[1280px]">
         <div className="mx-auto mt-8 w-4/5 rounded-lg bg-white p-4 md:mx-0 md:mt-0 md:w-1/2 lg:w-1/2">
           <PricingInfo components={components} />
-          <Form
-            className="mx-auto mb-4 flex w-2/3 max-w-80 flex-col rounded-lg border-4 border-gray-300 bg-gray-100 p-6"
-            action={submitPurchaseForm}>
-            <label className="text-base font-semibold">Job Boards</label>
-            <Input
-              className="mt-2 border-2 border-gray-500 md:text-base"
-              type="number"
-              value={postBoards}
-              onChange={(e) => {
-                if (Number(e.target.value) > 5) {
-                  setPostBoards(5);
-                } else if (Number(e.target.value) < 1) {
-                  setPostBoards(1);
-                } else {
-                  setPostBoards(Number(e.target.value));
-                }
-              }}
-            />
-
-            <label className="mt-2 text-base font-semibold">
-              Months Posted
-            </label>
-            <Input
-              className="mt-2 text-base md:text-base"
-              type="number"
-              value={postTime}
-              onChange={(e) => {
-                if (Number(e.target.value) > 12) {
-                  setPostTime(12);
-                } else if (Number(e.target.value) < 1) {
-                  setPostTime(1);
-                } else {
-                  setPostTime(Number(e.target.value));
-                }
-              }}
-            />
-
-            <label className="mt-2 text-base font-semibold">Total Cost</label>
-            <Input
-              className="gray-600 mt-2 text-base font-bold italic md:text-base"
-              type="string"
-              value={"$" + postTime * postBoards * 5.0 + ".00"}
-              readOnly
-            />
-          </Form>
         </div>
         <div className="mt-8 flex flex-col items-center md:ml-6 md:w-2/5 md:justify-center">
-          <div className="relative mx-auto my-6 w-fit rounded-lg border-4 border-gray-300 bg-gray-100 p-6 shadow-lg transition-all duration-500 hover:scale-105 hover:shadow-2xl sm:px-6">
-            <Form className="flex flex-col" action={submitPurchaseForm}>
-              <label className="mt-2 font-semibold mb:text-lg sm:text-xl">
+          <div className="relative mx-auto my-6 w-fit rounded-lg border-4 border-gray-300 bg-gray-100 p-6 sm:px-6">
+            <div className="flex flex-col">
+              <label className="mt-2 font-semibold mb:text-lg sm:text-lg">
                 Number Of Job Boards
               </label>
               <Input
@@ -135,7 +66,7 @@ export default function Page() {
                 }}
               />
 
-              <label className="mt-6 font-semibold mb:text-lg sm:text-xl">
+              <label className="mt-6 font-semibold mb:text-lg sm:text-lg">
                 Months Posted
               </label>
               <Input
@@ -153,22 +84,16 @@ export default function Page() {
                 }}
               />
 
-              <div
-                className={
-                  createSessionError ? "mx-auto mt-6 w-fit" : "hidden"
-                }>
-                <p className="text-center text-xl font-semibold text-red-500">
-                  <span className="text-2xl"> Error</span> <br />
-                  Please Try Again
-                </p>
-              </div>
-
-              <Button
-                type="submit"
-                className="mt-6 text-lg font-semibold sm:py-6 sm:text-xl">
-                Create New Post
-              </Button>
-            </Form>
+              <label className="mt-6 font-bold mb:text-lg sm:text-xl">
+                Total Cost
+              </label>
+              <Input
+                className="mt-2 border-2 border-gray-500 italic sm:text-lg md:text-lg"
+                type="string"
+                value={"$" + postTime * postBoards * 5.0 + ".00"}
+                readOnly
+              />
+            </div>
           </div>
         </div>
       </div>
