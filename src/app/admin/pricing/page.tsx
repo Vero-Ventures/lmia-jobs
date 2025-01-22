bc8b4819e32ee8cc9b26a3a7dc89d40c0f56d4b8
Michael Lei <michaelleie0ca@gmail.com>
2025-01-22 13:30:08 -0800

    Refactor pricing page

--- src/app/admin/pricing/page.tsx
+++ src/app/admin/pricing/page.tsx
@@ -6,7 +6,15 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { BaseLinks, SessionLinks } from "@/app/admin/dashboard/lib/constants";
 import { Input } from "@/components/ui/input";
-import PricingInfo from "@/app/admin/pricing/pricing-info.mdx";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 
 export default function Page() {
   const { data: session } = authClient.useSession();
@@ -14,45 +22,27 @@ export default function Page() {
   const [postBoards, setPostBoards] = useState(3);
   const [postTime, setPostTime] = useState(4);
 
-  const components = {
-    h1: (props: string[]) => (
-      <p
-        className="mb-2 mt-4 text-center text-xl font-bold mb:text-2xl sm:text-3xl"
-        {...props}
-      />
-    ),
-    h2: (props: string[]) => (
-      <p
-        className="mt-8 text-center text-xl font-bold mb:text-2xl sm:text-3xl"
-        {...props}
-      />
-    ),
-    h3: (props: string[]) => (
-      <p
-        className="mx-auto max-w-72 p-4 text-center text-xl font-semibold xl:max-w-full"
-        {...props}
-      />
-    ),
-    p: (props: string[]) => (
-      <p className="mt-4 px-6 text-lg leading-8" {...props} />
-    ),
-  };
-
   return (
     <div className="flex min-h-dvh flex-col">
       <Navbar links={session ? SessionLinks : BaseLinks} />
-      <div className="flex flex-grow flex-col justify-center md:flex-row md:justify-evenly md:px-6 md:py-8 lg:mx-auto lg:w-[1024px] xl:w-[1280px]">
-        <div className="mx-auto mt-8 w-4/5 rounded-lg bg-white p-4 md:mx-0 md:mt-0 md:w-1/2 lg:w-1/2">
-          <PricingInfo components={components} />
-        </div>
-        <div className="mt-8 flex flex-col items-center md:ml-6 md:w-2/5 md:justify-center">
-          <div className="relative mx-auto my-6 w-fit rounded-lg border-4 border-gray-300 bg-gray-100 p-6 sm:px-6">
-            <div className="flex flex-col">
-              <label className="mt-2 font-semibold mb:text-lg sm:text-lg">
-                Number Of Job Boards
-              </label>
+      <div className="mx-auto max-w-xl space-y-4 py-10">
+        <Card>
+          <CardHeader>
+            <CardTitle>Pricing</CardTitle>
+            <CardDescription>
+              The cost of an Opportunities job posting is based on its duration
+              and the number of boards it appears on. The base price is $5 per
+              month, multiplied by the number of boards selected.
+            </CardDescription>
+            <CardDescription>
+              For example, a post hosted on 5 boards for 3 months would cost
+              $75.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label>Number Of Job Boards</Label>
               <Input
-                className="mt-2 border-2 border-gray-500 sm:text-lg md:text-lg"
                 type="number"
                 value={postBoards}
                 onChange={(e) => {
@@ -65,12 +55,10 @@ export default function Page() {
                   }
                 }}
               />
-
-              <label className="mt-6 font-semibold mb:text-lg sm:text-lg">
-                Months Posted
-              </label>
+            </div>
+            <div className="space-y-2">
+              <Label>Months Posted</Label>
               <Input
-                className="mt-2 border-2 border-gray-500 sm:text-lg md:text-lg"
                 type="number"
                 value={postTime}
                 onChange={(e) => {
@@ -83,19 +71,15 @@ export default function Page() {
                   }
                 }}
               />
-
-              <label className="mt-6 font-bold mb:text-lg sm:text-xl">
-                Total Cost
-              </label>
-              <Input
-                className="mt-2 border-2 border-gray-500 italic sm:text-lg md:text-lg"
-                type="string"
-                value={"$" + postTime * postBoards * 5.0 + ".00"}
-                readOnly
-              />
             </div>
-          </div>
-        </div>
+          </CardContent>
+          <CardFooter>
+            <div className="space-y-2">
+              <h3 className="font-bold">Total Cost</h3>
+              <p>{"$" + postTime * postBoards * 5.0 + ".00"}</p>
+            </div>
+          </CardFooter>
+        </Card>
       </div>
       <Footer />
     </div>
