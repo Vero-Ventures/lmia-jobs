b06106aa6da431b45a84cd6547658f024208fd92
B. Rogers <bradenrogersdev@gmail.com>
2025-01-23 14:40:42 -0800

    Test simple page visit

--- src/actions/scraper/site-scrapers/job-bank.ts
+++ src/actions/scraper/site-scrapers/job-bank.ts
@@ -1,48 +1,48 @@
 import type { BrowserHandler } from "@/actions/scraper/scraping-handlers/browser-handler";
-import { CONFIG } from "../helpers/config";
+// import { CONFIG } from "../helpers/config";
 
 export async function scrapeGovJobBank(browserHandler: BrowserHandler) {
-  browserHandler.visitPage(CONFIG.urls.govSearchPage);
+  browserHandler.visitPage('https://www.facetofacegames.com/');
 
-  let pageNum = 1;
-  let scrape = true;
+  // let pageNum = 1;
+  // let scrape = true;
 
-  const postIds = [];
+  // const postIds = [];
 
-  while (scrape) {
-    postIds.push(await scrapePosts(browserHandler, pageNum));
-    pageNum += 1;
+  // while (scrape) {
+  //   postIds.push(await scrapePosts(browserHandler, pageNum));
+  //   pageNum += 1;
 
-    // Test Limit
-    if (pageNum > 1) {
-      scrape = false;
-    }
-  }
+  //   // Test Limit
+  //   if (pageNum > 1) {
+  //     scrape = false;
+  //   }
+  // }
 
-  console.log(postIds);
+  // console.log(postIds);
 }
 
-async function scrapePosts(
-  browserHandler: BrowserHandler,
-  pageNum: number
-): Promise<string[]> {
-  try {
-    const pagePostIds: string[] = [];
+// async function scrapePosts(
+//   browserHandler: BrowserHandler,
+//   pageNum: number
+// ): Promise<string[]> {
+//   try {
+//     const pagePostIds: string[] = [];
 
-    browserHandler.visitPage(CONFIG.urls.govSearchPage + String(pageNum));
+//     browserHandler.visitPage(CONFIG.urls.govSearchPage + String(pageNum));
 
-    const posts = await browserHandler.getElement(
-      CONFIG.selectors.govJobBank.jobPosting
-    );
+//     const posts = await browserHandler.getElement(
+//       CONFIG.selectors.govJobBank.jobPosting
+//     );
 
-    for (const post of await posts.all()) {
-      const fullId = await post.getAttribute("Id");
-      pagePostIds.push(fullId!.split("-")[1]);
-    }
+//     for (const post of await posts.all()) {
+//       const fullId = await post.getAttribute("Id");
+//       pagePostIds.push(fullId!.split("-")[1]);
+//     }
 
-    return pagePostIds;
-  } catch (error) {
-    console.error("Error getting job post Ids: " + error);
-    return [];
-  }
-}
+//     return pagePostIds;
+//   } catch (error) {
+//     console.error("Error getting job post Ids: " + error);
+//     return [];
+//   }
+// }
