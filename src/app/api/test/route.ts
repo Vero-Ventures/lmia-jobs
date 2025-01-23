927b45a8c1bd3985b7d28888735fac1d9665ba67
B. Rogers <bradenrogersdev@gmail.com>
2025-01-23 15:11:51 -0800

    Format result for proper return

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -2,8 +2,11 @@ import { runScraper } from "@/actions/scraper/run-scraper/setup";
 
 export async function GET() {
   const result = await runScraper();
+
+  const formattedResult = "[" + result.join(', ') + "]"
+
   if (result[0] !== "error") {
-    return result;
+    return new Response(formattedResult, { status: 400 });
   } else {
     return new Response("Error", { status: 400 });
   }
