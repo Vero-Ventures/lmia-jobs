5952f07cc6e439bea95add43bc8ea389f71172c0
B. Rogers <bradenrogersdev@gmail.com>
2025-01-24 10:24:45 -0800

    Fix routing display of result

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,10 +3,8 @@ import { runScraper } from "@/actions/scraper/run-scraper/setup";
 export async function GET() {
   const result = await runScraper();
 
-  const formattedResult = "[" + result.join(", ") + "]";
-
-  if (result[0] !== "error") {
-    return new Response(formattedResult, { status: 400 });
+  if (result.postEmails[0] !== "error" && result.postIds[0] !== "error") {
+    return new Response(JSON.stringify(result), { status: 400 });
   } else {
     return new Response("Error", { status: 400 });
   }
