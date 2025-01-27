1395a59a613b58f1603747daa67e8a008b73fece
B. Rogers <bradenrogersdev@gmail.com>
2025-01-27 11:59:07 -0800

    Fix test route error check

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,7 +3,7 @@ import { runScraper } from "@/actions/scraper/run-scraper/setup";
 export async function GET() {
   const result = await runScraper();
 
-  if (result.postEmails[0] !== "error" && result.postIds[0] !== "error") {
+  if (result.postEmails[0].email !== "error" && result.postIds[0] !== "error") {
     return new Response(JSON.stringify(result), { status: 400 });
   } else {
     return new Response("Error", { status: 400 });
