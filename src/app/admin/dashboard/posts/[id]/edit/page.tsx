d048b3521dfe8ab8365f1846ea09139b5b31f078
scottchen98 <scott.chen1117@gmail.com>
2025-01-27 17:31:16 -0800

    Refactor EditPost component to include detailed initial values with type definitions for job posting attributes

--- src/app/admin/dashboard/posts/[id]/edit/page.tsx
+++ src/app/admin/dashboard/posts/[id]/edit/page.tsx
@@ -3,6 +3,12 @@ import { JobPostForm } from "../../job-post-form";
 import { redirect } from "next/navigation";
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
+import type {
+  JobType,
+  Language,
+  PaymentType,
+  Province,
+} from "@/app/lib/constants";
 
 interface PageProps {
   params: Promise<{ id: string }>;
@@ -23,7 +29,32 @@ export default async function EditPost({ params }: PageProps) {
   });
   return (
     <div>
-      <JobPostForm mode="Edit" initialValues={jobPosting} />
+      <JobPostForm
+        mode="Edit"
+        initialValues={{
+          email: jobPosting.email,
+          jobTitle: jobPosting.jobTitle,
+          organizationName: jobPosting.organizationName,
+          province: jobPosting.region as Province,
+          city: jobPosting.city,
+          address: jobPosting.address || "",
+          startDate: jobPosting.startDate,
+          vacancies: jobPosting.vacancies || 0,
+          employmentType: jobPosting.employmentType as JobType,
+          workHours: jobPosting.workHours || 0,
+          paymentType: jobPosting.paymentType as PaymentType,
+          minPayValue: jobPosting.minPayValue,
+          maxPayValue: jobPosting.maxPayValue || 0,
+          description: jobPosting.description,
+          language: jobPosting.language as Language,
+          postAsylum: jobPosting.postAsylum,
+          postDisabled: jobPosting.postDisabled,
+          postIndigenous: jobPosting.postIndigenous,
+          postNewcomers: jobPosting.postNewcomers,
+          postYouth: jobPosting.postYouth,
+          monthsToPost: 1,
+        }}
+      />
     </div>
   );
 }
