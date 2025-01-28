a25e614826e0d6179a7023e0789b7ce5185adc3f
Michael Lei <michaelleie0ca@gmail.com>
2025-01-27 17:11:06 -0800

    Fetch post

--- src/app/admin/dashboard/posts/[id]/edit/page.tsx
+++ src/app/admin/dashboard/posts/[id]/edit/page.tsx
@@ -1,3 +1,29 @@
-export default function EditPost() {
-  return <div>page</div>;
+import { selectUserSingleJobPosting } from "@/db/queries/jobPostings";
+import { JobPostForm } from "../../job-post-form";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function EditPost({ params }: PageProps) {
+  const { id } = await params;
+  const data = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!data) {
+    redirect("/admin");
+  }
+  const jobPosting = await selectUserSingleJobPosting({
+    id,
+    userId: data.user.id,
+  });
+  return (
+    <div>
+      <JobPostForm mode="Edit" initialValues={jobPosting} />
+    </div>
+  );
 }
