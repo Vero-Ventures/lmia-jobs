21b74176f62339fd3d60e3322dfc8004f6a726fb
B. Rogers <bradenrogersdev@gmail.com>
2025-01-28 13:45:36 -0800

    Setting test

--- src/actions/scraper/site-scrapers/job-bank.ts
+++ src/actions/scraper/site-scrapers/job-bank.ts
@@ -469,7 +469,7 @@ async function getOtherJobDetails(browserHandler: BrowserHandler): Promise<{
 export async function getDescription(
   browserHandler: BrowserHandler
 ): Promise<string> {
-  const post = "43210916";
+  const post = "43183043";
 
   await browserHandler.visitPage(
     CONFIG.urls.searchResult + String(post) + "?source=searchresults"
