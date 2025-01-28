d36e96bfcde29383c2c5df702f0cd0f272e9144a
B. Rogers <bradenrogersdev@gmail.com>
2025-01-27 16:51:14 -0800

    Enviroment test with correct link

--- src/actions/scraper/site-scrapers/job-bank.ts
+++ src/actions/scraper/site-scrapers/job-bank.ts
@@ -478,21 +478,20 @@ export async function getDescription(
 ): Promise<string> {
   const description = "null";
 
+  const enviroment = await browserHandler.waitAndGetElement(
+    CONFIG.selectors.govJobBank.jobDetails.description.enviroment
+  );
+
+  console.log("Enviroment: " + (await enviroment.innerText()));
+
   const setting = await browserHandler.waitAndGetElement(
     CONFIG.selectors.govJobBank.jobDetails.description.setting
   );
 
   console.log("setting: " + (await setting.innerText()));
 
-  // const enviroment = await browserHandler.waitAndGetElement(
-  //   CONFIG.selectors.govJobBank.jobDetails.description.enviroment
-  // );
-
-  // console.log("Enviroment: " + (await enviroment.innerText()));
-
   // const credentials = await browserHandler.waitAndGetElement(
   //   CONFIG.selectors.govJobBank.jobDetails.description.credentials.container,
-  //   2500
   // );
 
   // const credentialsHeader = credentials.locator(
