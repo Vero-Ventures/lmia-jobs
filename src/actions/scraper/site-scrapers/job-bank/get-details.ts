6325829bc2a3458045f16a4ef3c65910c287ad1f
B. Rogers <bradenrogersdev@gmail.com>
2025-01-29 11:55:04 -0800

    Add description log to see formatted description display.

--- src/actions/scraper/site-scrapers/job-bank/get-details.ts
+++ src/actions/scraper/site-scrapers/job-bank/get-details.ts
@@ -45,6 +45,8 @@ export async function getJobDetails(
 
     const description = await getDescription(browserHandler);
 
+    console.log(description)
+
     const data = {
       postId,
       email: postEmail,
