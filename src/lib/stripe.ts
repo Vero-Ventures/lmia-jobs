97eed61b4dc17ff18eef2bb67f62dc42f355637e
Michael Lei <michaelleie0ca@gmail.com>
2025-01-30 17:52:19 -0800

    Finish stripe setup

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,5 +3,5 @@ import Stripe from "stripe";
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export function getUrl(path: string) {
-  return `${process.env.BETTER_AUTH_URL}${path}`;
+  return `${process.env.NODE_ENV === "production" ? "https://manageopportunities.ca" : "http://localhost:3000"}${path}`;
 }
