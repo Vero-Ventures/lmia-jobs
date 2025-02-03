12efd7a23fd42eae99c6916bc087625367546053
B. Rogers <bradenrogersdev@gmail.com>
2025-02-03 14:15:11 -0800

    Test basic monitoring route setup

--- src/app/api/monitor_posts/route.ts
+++ src/app/api/monitor_posts/route.ts
@@ -1,19 +1,18 @@
-import Parser from "rss-parser";
 // import { runScraper } from "@/actions/scraper/run-scraper/setup";
 
-const parser = new Parser();
-const RSS_FEED_URL =
-  "https://www.jobbank.gc.ca/jobsearch/feed/jobSearchRSSfeed?fet=%C2%AC1&fglo=1&fsrc=16&fage=2&sort=D&rows=100";
-
-export default async function handler() {
+export default async function handler(request: Request) {
   try {
-    const feed = await parser.parseURL(RSS_FEED_URL);
+    const headers = request.headers;
+    console.log(headers);
 
-    const latestPost = feed.items[0];
+    const params = request.url;
+    console.log(params);
+
+    const _monitorPostAuth = process.env.MONIOR_POST_AUTH;
 
     try {
-      console.log(latestPost)
-      // await runScraper(latestPost);
+      // console.log(monitorPostUrl);
+      // await runScraper(monitorPostUrl);
 
       return new Response("Completed", { status: 200 });
     } catch (error) {
