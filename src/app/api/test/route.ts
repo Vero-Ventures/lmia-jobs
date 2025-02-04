f40162b9df25189b4d32288af5dbea3a2629d7cf
B. Rogers <bradenrogersdev@gmail.com>
2025-02-04 11:28:37 -0800

    Test code based webhooking.

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,7 +1,12 @@
-import { runScraper } from "@/actions/scraper/run-scraper/setup";
+// import { runScraper } from "@/actions/scraper/run-scraper/setup";
 
-export async function GET() {
-  await runScraper("12345678");
+export async function GET(request: Request) {
+  // await runScraper("12345678");
+
+  const url = new URL(request.url);
+  const params = Object.fromEntries(url.searchParams.entries());
+
+  console.log("Query Parameters:", params);
 
   return new Response(JSON.stringify("Completed"), { status: 200 });
 }
