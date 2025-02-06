b493972502b5aa5674955d63118ce6b1ce88ed70
Michael Lei <michaelleie0ca@gmail.com>
2025-02-06 11:01:16 -0800

    Fix sign in styling

--- src/app/[jobBoard]/(auth)/sign-in/page.tsx
+++ src/app/[jobBoard]/(auth)/sign-in/page.tsx
@@ -1,5 +1,9 @@
 import SignIn from "./sign-in";
 
 export default function SignInPage() {
-  return <SignIn />;
+  return (
+    <div className="min-h-dvh content-center">
+      <SignIn />
+    </div>
+  );
 }
