7801d5ba58f85377ccc9ba94f5cbace9ab19d426
B. Rogers <bradenrogersdev@gmail.com>
2025-02-06 10:51:40 -0800

    Update sign in page elements.

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border-2 bg-card text-card-foreground shadow-sm",
       className
     )}
     {...props}
@@ -50,7 +50,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("overflow-clip text-sm text-muted-foreground", className)}
+    className={cn("overflow-clip text-muted-foreground", className)}
     {...props}
   />
 ));
