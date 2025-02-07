4dfb7e5958843a8cc9c9f003f38f72a53612c100
B. Rogers <bradenrogersdev@gmail.com>
2025-02-07 10:23:28 -0800

    Layout session check refactor.

--- src/app/[jobBoard]/dashboard/posts/create/page.tsx
+++ src/app/[jobBoard]/dashboard/posts/create/page.tsx
@@ -1,5 +1,8 @@
 import { type JobBoard } from "@/app/lib/constants";
 import { CreatePostForm } from "./create-post-form";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function CreatePostPage({
   params,
@@ -7,6 +10,14 @@ export default async function CreatePostPage({
   params: Promise<{ jobBoard: JobBoard }>;
 }) {
   const { jobBoard } = await params;
+
+  const data = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!data) {
+    redirect("/sign-in");
+  }
 
   return (
     <CreatePostForm
