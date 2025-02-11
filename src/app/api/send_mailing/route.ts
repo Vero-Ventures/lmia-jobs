c7cf017f32db8b12f3e01420c896bd69c854566a
B. Rogers <bradenrogersdev@gmail.com>
2025-02-11 09:13:15 -0800

    Format and comment api endpoints.

--- src/app/api/send_mailing/route.ts
+++ src/app/api/send_mailing/route.ts
@@ -1,6 +1,8 @@
 import { checkInactiveUserAges } from "@/actions/check-users";
 // import { mailInvitesAndReminders } from "@/actions/mailer";
 
+// Cron job to check for inactive users and send invites and reminders.
+// Set to run daily at 12:00 AM.
 export async function GET() {
   await checkInactiveUserAges();
   // await mailInvitesAndReminders();
--- src/app/api/test_scraper/route.ts
+++ src/app/api/test_scraper/route.ts
@@ -1,7 +1,7 @@
 import { runScraper } from "@/actions/scraper/run-scraper/setup";
 
-// The API route to run the scraper on a specific post.
-// Use: Testing the scraper on a post with specific content.
+// Test the scraper on a post with specific content.
+// Examples" Address, Max Pay, Max Work Hours, Or Specific Description Content.
 export async function GET() {
   const postId = "12345678";
 
