5436fbaf822bd6d9b3e26423b36de692eb42913e
B. Rogers <bradenrogersdev@gmail.com>
2025-03-04 08:24:39 -0800

    Disable mailer until further testing.

--- src/app/api/send_mailing/route.ts
+++ src/app/api/send_mailing/route.ts
@@ -1,11 +1,11 @@
 import { checkForInactiveUsers } from "@/actions/check-users";
-import { mailInvite } from "@/actions/mailer";
+// import { mailInvite } from "@/actions/mailer";
 
 // Cron job to check for inactive users and send out an invite.
 // Set to run hourly.
 export async function GET() {
   await checkForInactiveUsers();
-  await mailInvite();
+  // await mailInvite();
 
   return new Response("Completed", { status: 200 });
 }
