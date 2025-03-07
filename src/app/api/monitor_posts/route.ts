5e346797577344b967bbb7396bb15347424e54c2
B. Rogers <bradenrogersdev@gmail.com>
2025-03-07 15:26:44 -0800

    Remove logs from testing.

--- src/app/api/monitor_posts/route.ts
+++ src/app/api/monitor_posts/route.ts
@@ -8,56 +8,38 @@ export async function POST(request: Request) {
     // Body contains the authorization key and the post link.
     const text = await request.text();
 
-    console.log("Text")
-
-    console.log(text)
-
     const body = await JSON.parse(text);
-
-    console.log("Body")
-
-    console.log(body)
 
     // Check that the API request has the correct authorization value.
     if (
       !body.authorization ||
       body.authorization !== process.env.MONITOR_POST_AUTH
     ) {
-      console.log("Bad Auth")
       return new Response("Invalid monitor posts call authorization.", {
         status: 500,
       });
     }
 
-    console.log("Auth")
-
     // Check for a valid post link in the body.
     if (!body.postLink) {
-      console.log("No Link")
       return new Response("Post Link Not Found On Monitor Posts Call.", {
         status: 500,
       });
     } else {
       try {
-        console.log("Get ID")
         // Extract the post Id and run the scraper on the post.
         const postId = body.postLink.split("jobposting/")[1];
-
-        console.log("Post ID")
-        console.log(postId)
 
         await runScraper(postId);
 
         return new Response("Success.", { status: 200 });
       } catch (error) {
-        console.log("Scraper Error")
         return new Response("Failed to run scraper on RSS feed: " + error, {
           status: 500,
         });
       }
     }
   } catch (error) {
-    console.log("Other Error")
     return new Response("Failed to fetch RSS feed: " + error, {
       status: 500,
     });
